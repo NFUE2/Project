072e294b2f1304c4e35a5748d016758c2a9e0af5
NFUE2 <96811655+NFUE2@users.noreply.github.com>
2023-01-07 04:35:30 +0900

    Project

--- Project/Assets/Script/Camera/WorldMapCamera.cs
+++ Project/Assets/Script/Camera/WorldMapCamera.cs
@@ -4,27 +4,40 @@ using UnityEngine;
 
 public class WorldMapCamera : MonoBehaviour
 {
-    Transform Pos;
-    Vector2 ClickPos,EndPos;
+    public Vector3 Pos;
+    Vector2 ClickPos,MovePos;
     public float speed;
+    bool ClickChk;
+
     // Start is called before the first frame update
     void Start()
     {
-        Pos = transform;
-        //transform.position = GameObject.Find("Player").transform.transform.position + Pos.position;
+        Pos = transform.position;
+        ClickChk = false;
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-            ClickPos = Input.mousePosition;
 
-        if (Input.GetMouseButton(0))
-        {
-            EndPos = Input.mousePosition;
+        //if (Input.GetMouseButtonUp(0))
+        //    ClickChk = true;
 
-            Vector3 Move = EndPos - ClickPos;
-            transform.position -= new Vector3(Move.x, 0.0f, Move.y) * Time.deltaTime * speed;
-        }
+        //if(ClickChk)
+        //{
+        //    //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //    //if (Physics.Raycast(ray, out RaycastHit Hit))
+        //    //    transform.position = Vector3.Lerp(transform.position, transform.position + Hit.collider.gameObject.transform.position, Time.deltaTime);
+
+        //    //if (Vector3.Distance(transform.position, transform.position + Hit.collider.gameObject.transform.position) < 0.1f)
+        //    //    ClickChk = false;
+        //}
+        //else if (Input.GetMouseButton(0))
+        //{
+        //    //ClickChk = false;
+        //    //MovePos = Input.mousePosition;
+
+        //    //Vector3 Move = MovePos - ClickPos;
+        //    //transform.position -= new Vector3(Move.x, 0.0f, Move.y) * Time.deltaTime * speed;
+        //}
     }
 }
---
+++ Project/Assets/Script/Player/WorldMapPlayer.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldMapPlayer : MonoBehaviour
+{
+    public List<Vector3> Node;
+    Vector3[] Nodelist =
+    {
+        new Vector3(0.0f, 0.0f, 5.0f),
+        new Vector3(0.0f, 0.0f, -5.0f),
+        new Vector3(4.3f, 0.0f, 2.5f),
+        new Vector3(-4.3f, 0.0f, 2.5f),
+        new Vector3(4.3f, 0.0f, -2.5f),
+        new Vector3(-4.3f, 0.0f, -2.5f)
+    };
+
+
+    private static WorldMapPlayer Instance;
+    public static WorldMapPlayer GetInstance()
+    {
+        if (Instance == null)
+            Instance = new WorldMapPlayer();
+        return Instance;
+    }
+
+    private void Start()
+    {
+        Node = new List<Vector3>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray,out RaycastHit Hit) && Hit.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
+                Search(Hit.collider.gameObject.transform.position);
+        }
+    }
+
+    public void Search(Vector3 Pos)
+    {
+        Node.Clear();
+        Vector3 Cur_Pos = transform.position /*- new Vector3(0f,-1f,0f)*/;
+        Vector3 Next = Nodelist[0] + Cur_Pos;
+        Node.Add(transform.position);
+
+        //while (!Node.Contains(Pos))
+        for (int j = 0; j < 2; j++)
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                Vector3 CheckNode = Nodelist[i] + Cur_Pos;
+                Next = Vector3.Distance(Next, Pos) > Vector3.Distance(CheckNode, Pos) ? CheckNode : Next;
+            }
+            Cur_Pos = Next;
+            Node.Add(Next);
+        }
+
+        if (Node.Contains(Pos))
+        {
+            Debug.Log("완료");
+        }
+        Debug.Log(Pos);
+    }
+
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i < Node.Count - 1; i++)
+        {
+           Gizmos.DrawLine(Node[i],Node[i + 1]);
+        }
+    }
+}
